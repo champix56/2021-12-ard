5afc6885d75044dcfc54515afff5e7ef75e849ce
champix56 <desorbaix@free.fr>
2021-12-16 14:54:42 +0100

    c avancé avec union

--- structure/structure/main.c
+++ structure/structure/main.c
@@ -6,19 +6,58 @@
 //
 
 #include <stdio.h>
-
+struct S_gyro{
+    float gx,gy,gz;
+    float ix,iy,iz;
+};
+typedef struct S_gyro Gyro;
+//def de la forme du contenu
 struct S_meteo{
     float temp;
-    unsigned long pression;
-    unsigned int humidity;
+    uint32_t pression;
+    uint8_t humidity;
+};
+struct S_Datas{
+    struct S_meteo meteo;
+    Gyro gyro;
+};
+void setTemperture(struct S_meteo *m){
+    //(*m).temp=25.5;
+    m->temp=27.0F;
+    printf("in function -> %.2f;%6u;%2d\n",m->temp,m->pression,m->humidity);
+}
+void setGyro(struct S_Datas *d){
+    
+    d->gyro.gx=2.3F;
+    d->gyro.gy=3.3F;
+    d->gyro.gz=4.3F;
+}
+union U_S_Datas{
+    struct S_Datas s;
+    uint8_t b[sizeof(struct S_Datas)];
 };
 
-int main(int argc, const char * argv[]) {
-    struct S_meteo meteo;
-    meteo.temp=23.4F;
-    meteo.pression=102000ul;
-    meteo.humidity=43.0F;
+void viewUnionOfS_Data(struct S_Datas structData){
+    union U_S_Datas usd;
+    usd.s=structData;
     
-    printf("%.2f;%6ul;%2d\n",meteo.temp,meteo.pression,meteo.humidity);
+    for (int i=0;i<sizeof(struct S_Datas);i++) {
+        printf("%d;",usd.b[i]);
+    }
+    printf("\n");
+}
+int main(int argc, const char * argv[]) {
+    //def de l'instance de la struct
+    struct S_Datas datas;
+    datas.meteo.temp=12.0F;
+    datas.meteo.pression=102000ul;
+    datas.meteo.humidity=37;
+    setTemperture(&datas.meteo);
+    setGyro(&datas);
+    viewUnionOfS_Data(datas);
+    //affichage du contenu de la structure
+    printf("%.2f;%6u;%2d;%.3f;%.3f;%.3f\n",datas.meteo.temp,datas.meteo.pression,datas.meteo.humidity,datas.gyro.gx,datas.gyro.gy,datas.gyro.gz);
+    printf("%lu",sizeof(float));
     return 0;
 }
+
