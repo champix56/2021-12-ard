b57b367e710fe02032514a507cc8713bbf4c1814
champix56 <desorbaix@free.fr>
2021-12-16 11:13:09 +0100

    structure sans pointer

--- structure/structure/main.c
+++ structure/structure/main.c
@@ -7,20 +7,18 @@
 
 #include <stdio.h>
 
+struct S_meteo{
+    float temp;
+    unsigned long pression;
+    unsigned int humidity;
+};
+
 int main(int argc, const char * argv[]) {
-    float t1=37.1000F;
-    float t2=23.43F;
-    float t3=37.500003F;
-    float moyenne=0.0F;
-    int countTemp=3;
+    struct S_meteo meteo;
+    meteo.temp=23.4F;
+    meteo.pression=102000ul;
+    meteo.humidity=43.0F;
     
-    float allTemp[3]={t1,t2};
-    allTemp[2]=t3;
-    printf("Demat, Breizh!\n");
-    for (int i=0; i<countTemp;i++) {
-        moyenne=moyenne+allTemp[i];
-    }
-    moyenne=moyenne/(float)countTemp;
-    printf("Moyenne : %.2f\n",moyenne);
+    printf("%.2f;%6ul;%2d\n",meteo.temp,meteo.pression,meteo.humidity);
     return 0;
 }
