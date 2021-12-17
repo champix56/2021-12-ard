b95b4bcda29d2301404d46d6f2e0d50ee57c75f0
champix56 <desorbaix@free.fr>
2021-12-17 12:09:00 +0100

    debut ST7735

---
+++ projets/st7735DEMO/st7735DEMO.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  initDatas();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+void initDatas(){
+  s.meteo.pressure=101000;
+  s.meteo.temperature=40.54;
+  s.meteo.rhumidity=60.0F;
+  s.geo.alt=1000.0F;
+  s.tm.Hour=12;
+  s.tm.Minute=23;
+}
